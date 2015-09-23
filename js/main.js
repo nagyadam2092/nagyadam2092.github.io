a1b95175fa066afcc02a494605a926f9b0261cb8
Adam <nagyadam2092@gmail.com>
2015-09-23 09:04:54 +0200

    randomized blogger posts and git repos

--- js/main.js
+++ js/main.js
@@ -1,13 +1,46 @@
 $(document).ready(function(){
 	var blogId = "1762524982174490639";
 	var APIKey = "AIzaSyBskSuKFLJGb6GTu6DP03xeqyr3ehXJ6kI";
+	var posts = [];
 
 	$.when(
 		$.get("https://api.github.com/users/nagyadam2092/repos"),
 		$.get("https://www.googleapis.com/blogger/v3/blogs/" + blogId + "/posts?key=" + APIKey))
 		.done(function(git, blogger){
-			console.log(git);
-			console.log(blogger);
+			var gitPosts = git[0];
+			var bloggerPosts = blogger[0].items;
+			var $posts = $("#posts");
+			gitPosts.forEach(function(GPost){
+				var post = {};
+				post.title = GPost.name;
+				post.description = GPost.description;
+				post.link = GPost.url;
+				posts.push(post);
+			});
+			bloggerPosts.forEach(function(BPost){
+				var post = {};
+				post.title = BPost.title;
+				post.description = BPost.content;
+				post.link = BPost.url;
+				posts.push(post);
+			});
+			console.log(posts);
+			shuffle(posts);
+			posts.forEach(function(post){
+				$posts.append('<a href="' + post.link + '" target="_blank"><div class="col-md-4><h3>' + post.title + '</h3><p>' + post.description + '</p></div></a>')
+			});
+
+			function shuffle(array) {
+				var currentIndex = array.length, temporaryValue, randomIndex ;
+				while (0 !== currentIndex) {
+				randomIndex = Math.floor(Math.random() * currentIndex);
+				currentIndex -= 1;
+				temporaryValue = array[currentIndex];
+				array[currentIndex] = array[randomIndex];
+				array[randomIndex] = temporaryValue;
+				}
+				return array;
+			}	
 		});
 
 	/*$.get("https://api.github.com/users/nagyadam2092/repos", function(data){
